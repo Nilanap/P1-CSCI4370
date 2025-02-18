c105190f26e091e6e4d943ec31b29216a5daa997
nilanap <nap02617@uga>
2025-02-17 21:21:13 -0500

    Final Commit before submission (for review)

--- src/main/java/uga/cs4370/mydbimpl/Driver.java
+++ src/main/java/uga/cs4370/mydbimpl/Driver.java
@@ -19,29 +19,61 @@ public class Driver {
                 .attributeTypes(List.of(Type.INTEGER, Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
         instructor.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/instructor_export.csv");
-        System.out.println("Instructor Relation:");
-        instructor.print();
         Relation department = new RelationBuilder()
                 .attributeNames(List.of("Department", "Building Name", "Budget"))
                 .attributeTypes(List.of(Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
         department.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/department_export.csv");
-        System.out.println("Department Relation:");
-        department.print();
         Relation student = new RelationBuilder()
                 .attributeNames(List.of("Student_ID", " Last Name", "Major", "Credit Hours"))
                 .attributeTypes(List.of(Type.DOUBLE, Type.STRING, Type.STRING, Type.DOUBLE))
                 .build();
         student.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/student_export.csv");
+        Relation advisor = new RelationBuilder()
+            .attributeNames(List.of("Student_ID", "Instructor_ID"))
+            .attributeTypes(List.of(Type.DOUBLE, Type.DOUBLE))
+            .build();
+        advisor.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/advisor_export.csv");
+        
+  
 
 
 
 
 
 
+       
+        System.out.println("Query 1 (Bryce Wellman): Students who have completed more than 90 total credits in departments with a budget > $400,000.");
+        System.out.println("Query 2 (Nilan Patel): instructors who make over 70k in departments with a budget > $100000");
+        System.out.println("Query 3 (Michael Scott): Find the name of each instructor who teaches a section that is located in the building of the department the instructor belongs to, as well as which section that is (course and section IDs, year, and seemster) and what building it is in.");
+        System.out.println("Query 4 (Matthew Griffith): Find the names of Students who have taken over 125 credit hours and the IDs of their assigned advisors.");
+        System.out.println("Query 5: (Not used) Instructors who's salary totals to at least 1/8th of their departments yearly budget");
 
-        System.out.println("Query 1: instructors who make over 70k in departments with a budget > $100000");
-        System.out.println("Instructors earning more than the average salary in high-budget departments");
+
+        System.out.println("");
+
+        Relation studentFix = ra.rename(student, List.of(" Last Name", "Credit Hours"), List.of("Last_Name", "Credit_Hours"));
+
+        Predicate creditHoursAbove128 = row -> {
+            double creditHours = row.get(3).getAsDouble(); 
+            return creditHours > 128;
+        };
+        Relation highCreditStudents = ra.select(studentFix, creditHoursAbove128);
+
+        Predicate budgetAbove900k = row -> {
+            double budget = row.get(2).getAsDouble(); 
+            return budget > 900000;
+        };
+        Relation wellFundedDepartments = ra.select(department, budgetAbove900k);
+
+        Relation joinedResu = ra.join(highCreditStudents, wellFundedDepartments);
+
+        List<String> projectedAtt = List.of("Last_Name", "Student_ID", "Credit_Hours", "Major", "Department", "Budget");
+        Relation query2Result = ra.project(joinedResu, projectedAtt);
+
+        System.out.println("Bryce Wellman Query: Students who have completed more than 128 total credits in departments with a budget > $900,000");
+        query2Result.print();
+
 
         Predicate salaryGreaterThan70k = row -> {
             double salary = row.get(3).getAsDouble(); 
@@ -49,7 +81,7 @@ public class Driver {
         };
         Relation highSalaryInstructors = ra.select(instructor, salaryGreaterThan70k);
         Predicate budgetGreaterThan100k = row -> {
-            double budget = row.get(2).getAsDouble(); // Budget is the 3rd attribute in department.
+            double budget = row.get(2).getAsDouble(); 
             return budget > 100000;
         };
         Relation highBudgetDepartments = ra.select(department, budgetGreaterThan100k);
@@ -58,13 +90,99 @@ public class Driver {
         List<String> projectedAttrs = List.of("Name", "Department", "Salary", "Building Name", "Budget");
         Relation query1Result = ra.project(joinResult, projectedAttrs);
 
-        System.out.println("Query 1: Instructors who make over 70k in departments with a budget > $100,000");
+        System.out.println("Nilan Query Result: Instructors who make over 70k in departments with a budget > $100,000");
         query1Result.print();
 
+        queryMichael(ra);
 
 
-       
-     
+        System.out.println("Matthew Query Result: Find the names of Students who have taken over 125 credit hours and the IDs of their assigned advisors.");
+
+        Relation studentFixed = ra.rename(student, List.of(" Last Name", "Credit Hours"), List.of("Last_Name", "Credit_Hours"));
+
+        Predicate creditHoursAbove125 = row -> {
+            double creditHours = row.get(3).getAsDouble(); 
+            return creditHours > 125;
+        };
+        Relation eligibleStudents = ra.select(studentFixed, creditHoursAbove125);
+
+        Relation advisors = ra.join(eligibleStudents, advisor);
+
+        Relation finalResult = ra.project(advisors, List.of("Last_Name", "Instructor_ID"));
+        finalResult.print();
+
+
+
+
+
+        
+        
+    }
+
+    // Test SQL query: SELECT name, course_id, sec_id, year, building FROM (((SELECT ID, name, building FROM ((SELECT ID, name, dept_name FROM instructor) iT NATURAL JOIN (SELECT dept_name, building FROM department) dT)) _ NATURAL JOIN teaches)) NATURAL JOIN section ORDER BY ID, course_id;
+    private static final String QUERY_MICHAEL = "Michael Query: Find the name of each instructor who teaches a section that is located in the building of the department the instructor belongs to, as well as which section that is (course and section IDs, year, and seemster) and what building it is in.";
+    private static void queryMichael(RA ra) {
+        System.out.println(QUERY_MICHAEL);
+
+        Relation instructor = new RelationBuilder()
+            .attributeNames(List.of("ID", "name", "dept_name", "salary"))
+            .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.DOUBLE))
+            .build();
+        Relation department = new RelationBuilder()
+            .attributeNames(List.of("dept_name", "building", "budget"))
+            .attributeTypes(List.of(Type.STRING, Type.STRING, Type.DOUBLE))
+            .build();
+        Relation teaches = new RelationBuilder()
+            .attributeNames(List.of("ID", "course_id", "sec_id", "semester", "year"))
+            .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.STRING, Type.INTEGER))
+            .build();
+        Relation section = new RelationBuilder()
+            .attributeNames(List.of("course_id", "sec_id", "semester", "year", "building", "room_number", "time_slot_id"))
+            .attributeTypes(List.of(Type.STRING, Type.STRING, Type.STRING, Type.INTEGER, Type.STRING, Type.STRING, Type.STRING))
+            .build();
+        
+        instructor.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/instructor_export.csv");
+        department.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/department_export.csv");
+        section.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/section_export.csv");
+        teaches.loadData("/Users/nilanpatel/Desktop/Junior Year /Junior Year S2/DataBase Mgmtn CSCI 4370/teaches_export.csv");
+
+        
+        // Just the attributes we need from instructor
+        // [ID, name, dept_name]
+        Relation instructorTrim = ra.project(instructor, List.of("ID", "name", "dept_name"));
+
+        // Just the attributes we need from department
+        // [dept_name, building]
+        Relation departmentTrim = ra.project(department, List.of("dept_name", "building"));
+
+        // Each instructor's ID and name linked to the department they belong to and the building that department uses
+        // [ID, name, dept_name, building]
+        Relation instrWithDept = ra.join(instructorTrim, departmentTrim);
+
+        // We no longer need dept_name, we only care about the building and the instructor.
+        // We now have every instructor's ID and name linked to the building of the department they belong to.
+        // [ID, name, building]
+        Relation instrWithDeptTrim = ra.project(instrWithDept, List.of("ID", "name", "building"));
+
+        // We now have each instructor's ID, name, department building linked with every section they have ever taught.
+        // [ID, name, building, course_id, sec_id, semester, year]
+        Relation instrWithTeaches = ra.join(instrWithDeptTrim, teaches);
+
+        // Link each instructor/building/section key combo to the other info about each section
+        // [ID, name, building, course_id, sec_id, semester, year, room_number, time_slot_id]
+        Relation instrWithSectionSameBuilding = ra.join(instrWithTeaches, section);
+
+        // Get the attributes we care about
+        Relation result = ra.project(instrWithSectionSameBuilding, 
+            List.of("name", "course_id", "sec_id", "year", "building"));
+        
+        result.print();
+
+
+    }
+
+
+
 
         /* 
         // Test Union
@@ -177,7 +295,5 @@ public class Driver {
             System.out.println("\nError: " + e.getMessage());
         }
             */
-    }
 
 }
-
